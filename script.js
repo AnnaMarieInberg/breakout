ab5e01564e22fa90e9be9a4623da06d0695c50e3
AnnaMarieInberg <111721@emmauscollege.nl>
2020-10-05 07:19:38 +0000

    game

--- script.js
+++ script.js
@@ -1,27 +1,25 @@
-class 
 
-/**
- * setup
- * de code in deze functie wordt één keer uitgevoerd door
- * de p5 library, zodra het spel geladen is in de browser
- */
 function setup() {
-  // Maak een canvas (rechthoek) waarin je je speelveld kunt tekenen
+  
   createCanvas(1280, 720);
-
-}
-
-
-/**
- * draw
- * de code in deze functie wordt meerdere keren per seconde
- * uitgevoerd door de p5 library, nadat de setup functie klaar is
- */
-function draw() {
-  
-  // Kleur de achtergrond blauw, zodat je het kunt zien
   background('blue');
-   
-  
+}
+ 
+class SpelElement {
+    x;
+    y;
+
+
+constructor (_x,_y)
+    _x=x;
+    _y=y;
 }
 
+class Ball extends SpelElement {
+    speedX;
+    speedY;
+
+constructor(_speedX,_speedY)
+    _speedX=speedX;
+    _speedY=speedY;
+}
